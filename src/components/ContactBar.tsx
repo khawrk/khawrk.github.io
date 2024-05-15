432798b3fa716a8d9a42ac84f124ec141b1ef18a
Emika <khaw.rkk@gmail.com>
2024-05-15 12:06:29 -0700

    fix margin and responsive display

--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -1,7 +1,7 @@
 
 const ContactBar = () => {
   return (
-    <div className="absolute sm:left-0 bottom-0 left-0 sm:w-[50px] pb-5 sm:pb-0 h-screen justify-center sm:items-center items-end sm:pl-4 pl-0 sm:flex-col flex-row w-full gap-4 flex ">
+    <div className="mb-5 absolute sm:left-0 bottom-0 left-0 sm:w-[50px]  sm:pb-0 h-[90vh] justify-center sm:items-center items-end sm:pl-4 pl-0 sm:flex-col flex-row w-full gap-4 flex ">
       <a href="mailto:khaw.rkk@gmail.com" target="_blank">
         <div className='w-[30px] h-[30px] bg-mailNormal hover:bg-mailHover' id='icon'></div>
       </a>
